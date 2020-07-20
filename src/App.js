83f7857529f12a0e31b7b7f9c018210032f7465b
Scott <Scott@SCOTT-PC>
2020-07-20 13:43:29 -0700

    2nd pass

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import List from './List'
+import './styles.css';
 
-function App() { 
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  ); 
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render() {
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
-function Card(){
-  return(
-    <main className='Card'>
-      {}
-    </main>
-  );
+function List(key, header, cards){
+  document.getElementById("root");
+  ReactDOM.render(document.getElementById("root"));
 }
-
-function List(){
-  return(
-    <main className='List'>
-      {}
-    </main>
-  )
-}
-
 
 export default App;
+
+// import React from 'react';
+// import STORE from './store';
+
+// function App(store) { 
+//   return (
+//     <main className='App'>
+//       <List cards={store.lists}/>
+//     </main>
+//   ); 
+// }
+
+// function Card(title, content){
+//   return(
+//     <div className='Card'>
+//       <button type="button">Delete</button>
+//       <h3>Card</h3>
+//       <p>Info</p>
+//     </div>
+//   );
+// }
+
+// function List(cards){
+//   return <div className="list-cards">
+//     <GetCards cards={cards}/>
+//   </div>
+// }
+
+// function GetCards(cards) {
+//   console.log(cards);
+//   return (cards.map(card => function() {
+//     return <Card />;
+//   }));
+// }
+
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import STORE from './store';
+
+ReactDOM.render(<App store={STORE}/>, document.getElementById('root'));
