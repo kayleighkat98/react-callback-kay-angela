5bcdd0981ba9da487fd9dc44073025190408cab5
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-20 14:32:46 -0500

    loremipsum

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function App() { 
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  ); 
+}
+
+function Card(){
+  return(
+    <main className='Card'>
+      {}
+    </main>
+  );
+}
+
+function List(){
+  return(
+    <main className='List'>
+      {}
+    </main>
+  )
+}
+
+
+export default App;
