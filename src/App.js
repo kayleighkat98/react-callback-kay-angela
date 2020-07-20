3fc626d20a485f285c0f62c218e9d8626d65de16
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-20 16:21:16 -0500

    almost there

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-//import List from './List'
-import './styles.css';
+import List from './List'
+import './App.css';
 
 class App extends Component {
   static defaultProps = {
@@ -31,46 +31,4 @@ class App extends Component {
   }
 }
 
-function List(key, header, cards){
-  document.getElementById("root");
-  ReactDOM.render(document.getElementById("root"));
-}
-
 export default App;
-
-// import React from 'react';
-// import STORE from './store';
-
-// function App(store) { 
-//   return (
-//     <main className='App'>
-//       <List cards={store.lists}/>
-//     </main>
-//   ); 
-// }
-
-// function Card(title, content){
-//   return(
-//     <div className='Card'>
-//       <button type="button">Delete</button>
-//       <h3>Card</h3>
-//       <p>Info</p>
-//     </div>
-//   );
-// }
-
-// function List(cards){
-//   return <div className="list-cards">
-//     <GetCards cards={cards}/>
-//   </div>
-// }
-
-// function GetCards(cards) {
-//   console.log(cards);
-//   return (cards.map(card => function() {
-//     return <Card />;
-//   }));
-// }
-
-
-// export default App;
