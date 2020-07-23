6e6812bf7b556f05223947eea253099cad0ecf83
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-23 11:42:32 -0500

    delete uneccesary files

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              // onDeleteItem={list.props.onDeleteItem}
             />
           ))}
         </div>
